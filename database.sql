586ef14715ae528d38012d171f2ab67a3985a49b
solo7playz <148128819+solo7playz@users.noreply.github.com>
2026-04-28 12:27:18 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -10,11 +10,9 @@ CREATE TABLE Cities (
     FOREIGN KEY (countryid) REFERENCES Countries(id) ON DELETE CASCADE
 );
 
--- Наполним данными для теста
 INSERT INTO Countries (country) VALUES ('Украина'), ('Германия'), ('Франция');
 INSERT INTO Cities (city, countryid) VALUES ('Киев', 1), ('Львов', 1), ('Берлин', 2), ('Мюнхен', 2), ('Париж', 3);
 
--- Задание 2: Пользователи (если таблицы еще нет)
 CREATE TABLE Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     login VARCHAR(64) NOT NULL UNIQUE,
